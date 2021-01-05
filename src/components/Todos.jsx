4e4d4eca9e8ea3983d6ed56bc5de04dd9ca69895
Pauline Dussart <paulinedussart04@gmail.com>
2021-01-05 13:44:03 +0100

    index GET Request with axios DONE

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const axios = require('axios');
+
 
 export default function Todos() {
+const [todos, SetTodos] = useState()
+
+	useEffect(() => {
+		axios.get('https://cors-anywhere.herokuapp.com/https://pau16-todolist-api.herokuapp.com/api/v1/todos', {
+			method: "GET",
+			mode: "no-cors",
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Origin": "*",
+			},
+		})
+			.then(function (response) {
+				// handle success
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			})
+	}, [])
+
 	return (
-		<h1 className="text-3xl">Task Manager</h1>
+		<h1 className="font-serif font-black text-3xl">Task Manager</h1>
+
 	)
 }
